3f1cc0dc784712d2d91a194498904a67c86c0ab1
baxtersstocks <baxtersstocks@gmail.com>
2025-12-29 03:06:02 -0500

    Delete API/index.js

--- API/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({
-    status: "ok",
-    message: "SummifyAI backend is running"
-  });
-}
